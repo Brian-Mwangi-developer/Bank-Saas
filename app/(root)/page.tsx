1a2882afaf99be21a9229e2ea3f212da969ca2aa
Brian Mwangi <britabyzk29@gmail.com>
2024-07-12 13:51:12 +0300

    Right side and Bank Cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,32 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const loggedIn = { firstName: "Brian", lastName:"Smith", email: "brian@gmail.com"};
+    return (
+        <section className='home'>
+            <div className='home-content'>
+                <header className='home-header'>
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || "Guest"}
+                        subtext="Access and manage your Account transaction efficiently. " />
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.40} />
+                </header>
+                RECENT TRANSACTIONS
+            </div>
+            <RightSideBar
+            user={loggedIn}
+            transactions={[]}
+            banks={[{currentBalance: 125.70},{currentBalance:500}]} />
+        </section>
+    )
 }
 
 export default Home
